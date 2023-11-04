1095a4e9e14a554acb43d266c0f510cf3e5401d3
bhuwan <bkspoudel@gmail.com>
2023-11-04 15:05:58 +0100

    play around with json-server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BlogService } from './services/blog.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'blog';
+  allPosts = this.blogService.getAllPosts();
+  constructor(private blogService: BlogService) {}
+
+  ngOnInit(): void {
+    this.allPosts = this.blogService.getAllPosts();
+    this.allPosts.subscribe((post) => {
+      console.log('postId:', post?.id);
+    });
+  }
 }
--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BlogService {
+  apiUrl = 'http://localhost:5000/posts';
 
-  constructor() { }
+  constructor(private _http: HttpClient) {}
+
+  getAllPosts(): Observable<{ id: number; title: string; author: string }> {
+    return this._http.get<{ id: number; title: string; author: string }>(
+      this.apiUrl
+    );
+  }
 }
